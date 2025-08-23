dbaba37781fe6643a5d41fd1a0b6723b2f10bd4b
aedmond1122-bit <aedmond1122@gmail.com>
2025-08-23 06:26:46 +0000

    Add all project files and update pages: sea-freight, services, and thank-you

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./prime-shipping-and-logistics-2020-main/**/*.html",
+    "./prime-shipping-and-logistics-2020-main/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
